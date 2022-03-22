dd574bc38be473213c033e7a80e6669e7beba3df
zayanahmed925 <rjraju925@gmail.com>
2022-03-23 01:57:19 +0600

    local storage added from cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../product/Product';
 import './Shop.css'
@@ -15,6 +16,7 @@ const Shop = () => {
         console.log(product)
         const newCart = [...cart, product]
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
