728870f0c184d476c102e8954d71e2ad838858d3
zayanahmed925 <rjraju925@gmail.com>
2022-03-23 01:33:28 +0600

    card all calculation compleate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,10 +3,23 @@ import './cart.css'
 const Cart = (props) => {
     const { cart } = props;
     console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
-        <div className=''>
-            <h1>This is Cart</h1>
+        <div className='cart'>
+            <h3>Order Summary</h3>
             <p>Selected Items: {props.cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: {shipping}</p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
+
         </div>
     );
 };
